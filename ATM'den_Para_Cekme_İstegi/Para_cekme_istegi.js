a5c40f1879dc31552c5e53a6962244cdf5b00ad8
Muhammed Ömer Müştekin <57271156+omermustekiin@users.noreply.github.com>
2022-03-06 17:57:41 +0300

    Add files via upload

---
+++ ATM'den_Para_Cekme_İstegi/Para_cekme_istegi.js
@@ -0,0 +1,50 @@
+// iki tane hesap oluştur içine bilgileri koy.
+// paracek adında fonksiyon oluştur, hesap ve miktar parametrelerini gir. ekrana hosgeldın yazdır
+//kontrol yapıları ile paranızı cekıp cekemeyecegımızı belirle ve yazdır.
+//bakiye yetersiz ise ek hesab sorusunu getir ve ona uuygun kontrol yapısı oluştur.
+
+
+var hesapA = {
+    ad: 'Ömer Müştekin',
+    bakiye: 2000,
+    hesapNo: '12312331',
+    ekHesap: 1000 
+}
+
+var hesapB = {
+    ad: 'Sevgi Yıldırım',
+    bakiye: 4000,
+    hesapNo: '54416094',
+    ekHesap: 2000 
+}
+
+function paraCek(hesap,miktar)
+{
+    console.log(`Hoşgeldiniz sayın ${hesap.ad}`);
+
+    if(hesap.bakiye >= miktar){
+        hesap.bakiye -= miktar;           //para çekme işlemi sonrası çekilen miktarın hesaptan düşmesi gerek.
+        console.log('Paranızı çekebilirsiniz.');
+    }
+    else{
+        var toplam = hesap.bakiye + hesap.ekHesap;
+
+        if(toplam >= miktar){
+            if(confirm(' Üzgünüz, ana hesabınızdaki bakiye yapmak istediğiniz işlem için yeterli değil. \n İşleme devam etmek için ek hesabınızı kullanmak ister misiniz?')){
+                console.log('Paranızı çekebilirsiniz');
+                var bakiye = hesap.bakiye;                 //bakiye bilgisini hesap içerisinden aldık
+                var ekhesap = miktar - bakiye;             //ek hesaptan ne kadar çekilecek, ne kadar ihtiyacımız var(miktardan bakiye çıkarsa belli olur)
+                hesap.bakiye = 0;                          //hesap objesinin bakiyesini sıfırlarız çunku hepsını kullanacagız.
+                hesap.ekhesap-=ekhesap;
+                }
+            else{
+                    console.log(`Üzgünüz, ${hesap.hesapNo} numaralı hesabınızda bu işlem için yeterli bakiye bulunmamaktadır`);  
+                }
+            }
+    else{
+        console.log('Üzgünüz, bakiye yetersiz'); }
+        }
+         
+}        
+paraCek(hesapA,2000);
+paraCek(hesapA,1000);
